974e84ee103502b5146499de91f505a83f43561c
Lokman Hossain <lokmanhossain@Lokmans-MacBook-Air.local>
2025-07-14 19:37:07 +0600

    db test2

--- src/main.ts
+++ src/main.ts
@@ -25,7 +25,7 @@ async function bootstrap() {
   .build();
 const document = SwaggerModule.createDocument(app, config);
 SwaggerModule.setup('api-docs', app, document);
-let port = process.env.PORT || 3000;
+let port = process.env.PORT;
   await app.listen(port);
   Logger.log(`${ await app.getUrl()}`)
 }
